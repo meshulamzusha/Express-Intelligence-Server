8d71e10f30cea24c46b41070c2058f1323a17481
meshulamzusha <nauko763@gmail.com>
2025-12-22 21:45:11 +0200

    refactor: remove unnecessary console log from user registration check

--- utils/usersUtils.js
+++ utils/usersUtils.js
@@ -10,8 +10,6 @@ export async function isUserRegistered(password) {
         if (!user) {
             registered = false
         }
-
-        console.log(registered);
         
         return registered
 
